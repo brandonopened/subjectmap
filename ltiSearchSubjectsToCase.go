efdd18dbdebd3a2c7505b38a8a0b3e028d34dc6b
Adam Blum <bluma@act.org>
2019-03-06 19:48:56 -0800

    generating CASE file now

--- ltiSearchSubjectsToCase.go
+++ ltiSearchSubjectsToCase.go
@@ -1,13 +1,16 @@
 // ltiSearchSubjectsToCase.go
 //
-// Loads JSON file with a subject taxonomy expressed in IMS Global LTI Resource Search 
+// Loads JSON file with a subject taxonomy expressed in IMS Global LTI Resource Search
 // Subjects taxonomy payload format
 //
 // Saves as valid IMS Global CASE format, which can be loaded into any CASE compliant
 // skill/standards manager
-// 
+//
+// Generates <basename>_case.json file
+//
 // Usage:
-//		go run ltiSearchSubjectsToCase.go <subjects json file> <base URI to use>
+//		go run ltiSearchSubjectsToCase.go <subjects basename file> <base URI to use>
+//
 
 package main
 
@@ -16,6 +19,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
+
 	"github.com/gofrs/uuid"
 )
 
@@ -33,7 +38,7 @@ type Subject struct {
 
 // CFItems struct
 type CFItems struct {
-	CFItems []CFItem
+	CFItems map[string]CFItem `json:"CFItems"`
 }
 
 // CFItem struct
@@ -55,12 +60,11 @@ const ( // iota is reset to 0
 	hasSkillLevel = iota
 )
 
-
 // LinkGenURI struct
 type LinkGenURI struct {
-	Title		string
-	Identifier  string
-	URI			string		
+	Title      string
+	Identifier string
+	URI        string
 }
 
 // CFAssociation struct
@@ -75,33 +79,66 @@ type CFAssociations struct {
 	CFAssociations []CFAssociation
 }
 
-
-func subjectsToCFItems(subjects Subjects,uriPrefix string) CFItems {
-	var cfItems CFItems
-
+func (m *CFItems) loadSubjects(subjects Subjects, uriPrefix string) int {
+	m.CFItems = make(map[string]CFItem)
 	for _, v := range subjects.Subjects {
-		uri := uriPrefix + "/" + v.Identifier
-		item := CFItem{Identifier: v.Identifier, HumanCodingScheme: v.Name, URI: uri}
-		cfItems.CFItems = append(cfItems.CFItems, item)
+		var cfItem CFItem
+		id, err := uuid.NewV1()
+		if err != nil {
+			fmt.Println("Can't create GUID")
+		}
+		cfItem.Identifier = id.String()
+		cfItem.HumanCodingScheme = v.Name
+		cfItem.URI = uriPrefix + "/" + id.String()
+		m.CFItems[v.Identifier] = cfItem
 	}
-	return cfItems
+	return len(m.CFItems)
 }
 
-func subjectsToChildCFAssociations(subjects Subjects,uriPrefix string) CFAssociations {
-	var cfAssociations CFAssociations
+func (m *CFAssociations) loadChildCFAssociations(subjects Subjects, uriPrefix string) int {
 	for _, v := range subjects.Subjects {
-		var orgURI,destURI LinkGenURI
+		var orgURI, destURI LinkGenURI
 
 		orgURI.Identifier = uuid.Must(uuid.NewV4()).String()
 		orgURI.URI = uriPrefix + "/" + v.Identifier
-		association := CFAssociation{OriginNodeURI:orgURI,DestinationNodeURI:destURI,AssociationType:isChildOf}
-		cfAssociations.CFAssociations=append(cfAssociations.CFAssociations,association)
+		association := CFAssociation{OriginNodeURI: orgURI, DestinationNodeURI: destURI, AssociationType: isChildOf}
+		m.CFAssociations = append(m.CFAssociations, association)
 	}
-	return cfAssociations
+	return len(m.CFAssociations)
 }
 
-func generateCASEFile(cfItems CFItems,cfAssociations CFAssociations) {
-	// TODO: perform CASE generation
+// CFDocument struct
+type CFDocument struct {
+	URI                string `json:"uri"`
+	CFPackageURI       string `json:"CFPackageURI"`
+	Identifier         string `json:"identifier"`
+	Creator            string `json:"creator"`
+	Title              string `json:"title"`
+	Description        string `json:"description"`
+	LastChangeDateTime string `json:"lastChangeDateTime"`
+}
+
+// Init ... set up the CFDocument structure
+func (m *CFDocument) Init(uriPrefix string, baseName string) {
+
+	id, err := uuid.NewV1()
+	if err != nil {
+		fmt.Println("Failed to create unique ID")
+	}
+	m.URI = uriPrefix + "/CFDocuments/" + id.String()
+	m.CFPackageURI = uriPrefix + "/CFPackages/" + id.String()
+	m.Identifier = id.String()
+	m.Creator = "S2S"
+	m.Title = baseName
+	m.Description = baseName
+	m.LastChangeDateTime = time.Now().Format("YYYY-MM-DDThh:mm:ss")
+}
+
+// CFPackage struct
+type CFPackage struct {
+	CFDocument     CFDocument     `json:"CFDocument"`
+	CFItems        CFItems        `json:"CFItems"`
+	CFAssociations CFAssociations `json:"CFAssociations"`
 }
 
 func main() {
@@ -112,22 +149,22 @@ func main() {
 	if len(os.Args) > 1 {
 		baseName = os.Args[1]
 	}
-	fileName := baseName + ".json"
+	subjectsJSONFileName := baseName + ".json"
 	// Open our jsonFile with the LTI Resource Search subject taxonomy payload
-	jsonFile, err := os.Open(fileName)
+	jsonFile, err := os.Open(subjectsJSONFileName)
 	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("Successfully opened %s\n", fileName)
+		fmt.Printf("Successfully opened %s\n", subjectsJSONFileName)
 	}
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
 	// now set a base name for all the URI identifiers
 	baseURI := "http://frameworks.act.org"
-	if len(os.Args) > 1 {
-		baseName = os.Args[2]
+	if len(os.Args) > 2 {
+		baseURI = os.Args[2]
 	}
 	caseSuffix := "/ims/case/v1/p0"
 	uriPrefix := baseURI + caseSuffix
@@ -141,18 +178,26 @@ func main() {
 	// we unmarshal our byteArray which contains our
 	// jsonFile's content into 'users' which we defined above
 	json.Unmarshal(byteValue, &subjects)
-	fmt.Printf("Finished loading %d subjects\n", len(subjects.Subjects))
 
 	var cfItems CFItems
-	cfItems = subjectsToCFItems(subjects,uriPrefix)
-	fmt.Printf("%d CFItems loaded\n", len(cfItems.CFItems))
-	jsonPayload := []byte("INIT")
-	jsonPayload, err = json.Marshal(cfItems)
-	s := string(jsonPayload[:])
-	fmt.Printf("JSON payload: %s\n", s)
-
+	count := cfItems.loadSubjects(subjects, uriPrefix)
+	fmt.Printf("Finished loading %d subjects\n", count)
 	var cfAssociations CFAssociations
-	cfAssociations = subjectsToChildCFAssociations(subjects,uriPrefix)
+	cfAssociations.loadChildCFAssociations(subjects, uriPrefix)
 
-	generateCASEFile(cfItems,cfAssociations)
+	var cfDocument CFDocument
+	cfDocument.Init(uriPrefix, baseName)
+
+	var cfPackage CFPackage
+	cfPackage.CFDocument = cfDocument
+	cfPackage.CFItems = cfItems
+	cfPackage.CFAssociations = cfAssociations
+
+	caseJSONFileName := baseName + "_case.json"
+	caseJSON, err := json.Marshal(cfPackage)
+	fmt.Println("Writing CASE to " + caseJSONFileName)
+	err = ioutil.WriteFile(caseJSONFileName, caseJSON, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
