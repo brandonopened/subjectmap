756c0491b5e9cb7294dc50b3e9b6a76bf400c436
Adam Blum <bluma@act.org>
2019-03-08 15:49:39 -0800

    sweet it generates CASE that loads

--- subjectsToCase.go
+++ subjectsToCase.go
@@ -19,6 +19,7 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/gocarina/gocsv"
@@ -65,7 +66,7 @@ type CFItem struct {
 	HumanCodingScheme  string `json:"humanCodingScheme"`
 	CFDocumentURI      string `json:"CFDocumentURI"`
 	Identifier         string `json:"identifier"`
-	FullStatement	   string `json:"fullStatement"`
+	FullStatement      string `json:"fullStatement"`
 	ConceptKeywords    string `json:"conceptKeywords"`
 	EducationLevel     string `json:"educationLevel"`
 	LastChangeDateTime string `json:"lastChangeDateTime"`
@@ -79,10 +80,38 @@ type LinkGenURI struct {
 }
 
 // CFAssociation struct
+/*
+{
+	"uri": "http://frameworks.act.org/uri/8e87f5ca-3bbc-11e9-8a0b-dd5472351291",
+	"identifier": "8e87f5ca-3bbc-11e9-8a0b-dd5472351291",
+	"lastChangeDateTime": "2019-03-01T00:54:24+00:00",
+	"CFDocumentURI": {
+	  "title": "ACT Holistic Framework - Mathematics V2.0",
+	  "identifier": "8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f",
+	  "uri": "http://frameworks.act.org/uri/8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f"
+	},
+	"originNodeURI": {
+	  "title": "H.A.MATH.NQ",
+	  "identifier": "01befe44-bc4c-11e8-a572-0242ac120003",
+	  "uri": "http://frameworks.act.org/uri/01befe44-bc4c-11e8-a572-0242ac120003"
+	},
+	"associationType": "isChildOf",
+	"destinationNodeURI": {
+	  "title": "ACT Holistic Framework - Mathematics V2.0",
+	  "identifier": "8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f",
+	  "uri": "http://frameworks.act.org/uri/8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f"
+	},
+	"sequenceNumber": 1
+  },
+  */
 type CFAssociation struct {
-	OriginNodeURI      LinkGenURI `json:"originNodeURI"`
-	DestinationNodeURI LinkGenURI `json:"destinationNodeURI"`
-	AssociationType    string     `json:"associationType"`
+	URI                	string    	`json:"uri"`
+	Identifier			string 	   	`json:"identifier"`
+	CFDocumentURI 		LinkGenURI	`json:"CFDocumentURI"`
+	OriginNodeURI     	LinkGenURI	`json:"originNodeURI"`
+	DestinationNodeURI 	LinkGenURI	`json:"destinationNodeURI"`
+	AssociationType    	string    	`json:"associationType"`
+	sequenceNumber		int		 	`json:"associationType"`
 }
 
 // CFAssociations struct
@@ -94,20 +123,29 @@ func (m *CFItems) loadSubjects(subjects Subjects, uriPrefix string, cfDocumentUR
 	m.CFItems = make(map[string]CFItem)
 	for _, v := range subjects.Subjects {
 		var cfItem CFItem
-		id, err := uuid.NewV1()
+		id, err := uuid.NewV4()
 		if err != nil {
-			fmt.Println("Can't create GUID")
+			log.WithFields(log.Fields{"err": err.Error}).Info("No GUID for subject")
 		}
 
 		cfItem.Identifier = id.String()
-		if (len(v.Name)<1) {
+		if len(v.Name) < 1 {
 			v.Name = "(Unknown)"
 		}
-		cfItem.HumanCodingScheme = v.Name
+		const MaxHumanCodingScheme = 10
+		if len(v.Name) > MaxHumanCodingScheme {
+			cfItem.HumanCodingScheme = v.Name[0 : MaxHumanCodingScheme-1]
+		} else {
+			cfItem.HumanCodingScheme = v.Name
+		}
 		cfItem.FullStatement = v.Name
 		cfItem.URI = uriPrefix + "/" + id.String()
 		cfItem.CFDocumentURI = cfDocumentURI
-		cfItem.LastChangeDateTime = time.Now().Format("YYYY-MM-DDThh:mm:ss")
+		t := time.Now()
+		cfItem.LastChangeDateTime = fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+00:00",
+			t.Year(), t.Month(), t.Day(),
+			t.Hour(), t.Minute(), t.Second())
+
 		m.CFItems[v.Identifier] = cfItem
 	}
 	return len(m.CFItems)
@@ -116,36 +154,86 @@ func (m *CFItems) loadSubjects(subjects Subjects, uriPrefix string, cfDocumentUR
 // uses the supplementary subject info in the CSV to add additional metadata on each CFItem
 // currently populating ConceptKeywords and EducationLevel
 func (m *CFItems) loadSubjectsInfo(subjectsInfo SubjectsInfo, subjects Subjects) {
-	var i CFItem 
+	var i CFItem
 	for _, v := range subjectsInfo.SubjectsInfo {
-		i=m.CFItems[v.NodeID]
+		i = m.CFItems[v.NodeID]
 		i.ConceptKeywords = v.Keyword
 		i.EducationLevel = v.Grades
-		m.CFItems[v.NodeID]=i
+		m.CFItems[v.NodeID] = i
 	}
 }
 
 // creates CASE isChildOf CFAssociations based on the parent-child relationships in the subject taxoonomy
 // returns the number of associations created
-func (m *CFAssociations) loadChildren(subjects Subjects, cfItems CFItems, uriPrefix string) int {
+// this is a sample top level isChildOf from ACT Holistic Framework Math
+// NOTE THAT WE CREATE ISCHILDOF OF THE CFDOCUMENT IDENTIFIER
+/*
+    {
+      "uri": "http://frameworks.act.org/uri/8e87f5ca-3bbc-11e9-8a0b-dd5472351291",
+      "identifier": "8e87f5ca-3bbc-11e9-8a0b-dd5472351291",
+      "lastChangeDateTime": "2019-03-01T00:54:24+00:00",
+      "CFDocumentURI": {
+        "title": "ACT Holistic Framework - Mathematics V2.0",
+        "identifier": "8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f",
+        "uri": "http://frameworks.act.org/uri/8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f"
+      },
+      "originNodeURI": {
+        "title": "H.A.MATH.NQ",
+        "identifier": "01befe44-bc4c-11e8-a572-0242ac120003",
+        "uri": "http://frameworks.act.org/uri/01befe44-bc4c-11e8-a572-0242ac120003"
+      },
+      "associationType": "isChildOf",
+      "destinationNodeURI": {
+        "title": "ACT Holistic Framework - Mathematics V2.0",
+        "identifier": "8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f",
+        "uri": "http://frameworks.act.org/uri/8b5f90a6-3bbc-11e9-9fd0-97d95d55a29f"
+      },
+      "sequenceNumber": 1
+    },
+*/
+func (m *CFAssociations) loadChildren(subjects Subjects, cfItems CFItems, uriPrefix string, cfDocument CFDocument) int {
 	for _, v := range subjects.Subjects {
+
+		var cfDocumentURI LinkGenURI
+		cfDocumentURI.Identifier = cfDocument.Identifier
+		cfDocumentURI.Title = cfDocument.Title
+		cfDocumentURI.URI = cfDocument.URI
 		var orgURI LinkGenURI
-		var destURI LinkGenURI
 		var orgItem CFItem
 		orgItem = cfItems.CFItems[v.Identifier]
 		orgURI.Identifier = orgItem.Identifier
 		orgURI.URI = uriPrefix + "/" + orgURI.Identifier
-		orgURI.Title = v.Name
+		orgURI.Title = cfItems.CFItems[v.Identifier].FullStatement
+		var destURI LinkGenURI
 		var destItem CFItem
-		destItem = cfItems.CFItems[v.Parent]
-		destURI.Identifier = destItem.Identifier
-		destURI.URI = uriPrefix + "/" + destURI.Identifier
-		destURI.Title = v.Name
+		if (v.Parent=="0") {
+			destURI.Identifier = cfDocument.Identifier
+			destURI.URI = cfDocument.URI
+			destURI.Title = cfDocument.Title 
+		} else {
+			destItem = cfItems.CFItems[v.Parent]
+			destURI.Identifier = destItem.Identifier
+			destURI.URI = uriPrefix + "/" + destURI.Identifier
+			destURI.Title = cfItems.CFItems[v.Parent].FullStatement
+		}
+
 		var cfAssociation CFAssociation
+
+		id, err := uuid.NewV4()
+		if err != nil {
+			log.WithFields(log.Fields{"err": err.Error}).Info("Can't get UUID for association")
+		}
+		cfAssociation.URI = uriPrefix + "/cfAssociations/" + id.String()
+		cfAssociation.Identifier = id.String()
+		cfAssociation.CFDocumentURI = cfDocumentURI
 		cfAssociation.OriginNodeURI = orgURI
 		cfAssociation.DestinationNodeURI = destURI
 		cfAssociation.AssociationType = "isChildOf"
+		// TODO: computer sequence number
+		//cfAssociation.sequenceNumber = 1
 		m.CFAssociations = append(m.CFAssociations, cfAssociation)
+
+
 	}
 	return len(m.CFAssociations)
 }
@@ -164,10 +252,11 @@ type CFDocument struct {
 // Init ... set up the CFDocument structure
 func (m *CFDocument) Init(uriPrefix string, baseName string) {
 
-	id, err := uuid.NewV1()
+	id, err := uuid.NewV4()
 	if err != nil {
-		fmt.Println("Failed to create unique ID")
+		log.WithFields(log.Fields{"err": err.Error}).Info("No GUID for subject")
 	}
+
 	m.URI = uriPrefix + "/CFDocuments/" + id.String()
 	m.CFPackageURI = uriPrefix + "/CFPackages/" + id.String()
 	m.Identifier = id.String()
@@ -179,9 +268,9 @@ func (m *CFDocument) Init(uriPrefix string, baseName string) {
 
 // CFPackage struct
 type CFPackage struct {
-	CFDocument     CFDocument     `json:"CFDocument"`
-	CFItems        CFItems        `json:"CFItems"`
-	CFAssociations CFAssociations `json:"CFAssociations"`
+	CFDocument     CFDocument        `json:"CFDocument"`
+	CFItems        map[string]CFItem `json:"CFItems"`
+	CFAssociations []CFAssociation   `json:"CFAssociations"`
 }
 
 func main() {
@@ -224,7 +313,7 @@ func main() {
 	count := cfItems.loadSubjects(subjects, uriPrefix, cfDocument.URI)
 	log.WithFields(log.Fields{"count": count}).Info("Loaded subjects")
 
-	// load supplementary information about subjects from CSV file 
+	// load supplementary information about subjects from CSV file
 	subjectsInfoFileName := baseName + ".csv"
 	subjectsInfoFile, err := os.OpenFile(subjectsInfoFileName, os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
@@ -245,15 +334,15 @@ func main() {
 	// now use the parent child relationships in the subjects.json
 	// to generate CASE "isChildOf" associations
 	var cfAssociations CFAssociations
-	cfAssociations.loadChildren(subjects, cfItems, uriPrefix)
+	cfAssociations.loadChildren(subjects, cfItems, uriPrefix,cfDocument)
 
 	var cfPackage CFPackage
 	cfPackage.CFDocument = cfDocument
-	cfPackage.CFItems = cfItems
-	cfPackage.CFAssociations = cfAssociations
+	cfPackage.CFItems = cfItems.CFItems
+	cfPackage.CFAssociations = cfAssociations.CFAssociations
 
 	caseJSONFileName := baseName + "_case.json"
-	caseJSON, err := json.Marshal(cfPackage)
+	caseJSON, err := json.MarshalIndent(cfPackage,"","    ")
 	err = ioutil.WriteFile(caseJSONFileName, caseJSON, 0644)
 	if err != nil {
 		log.WithFields(log.Fields{"file": caseJSONFileName}).Fatal("Can't write CASE file")
