fe53fa4fdb9621e586b7b6370dc914fe182f2a78
Adam Blum <bluma@act.org>
2019-03-20 19:47:13 -0700

    sorted results by relevance

--- suggestmap/suggestMap.go
+++ suggestmap/suggestMap.go
@@ -1,11 +1,24 @@
+// subjectMap.go
+//
+// Given a set of items (skills/standards) loaded into OpenSALT
+// Returns a list of items by similarity to a supplied phrase
+//
+// Generates <basename>_case.json file
+//
+// Usage:
+//		go run subjectMap.go <phrase>
+//
 package main
 
 import (
 	"database/sql"
 	"os"
+	"strings"
 
+	slice "github.com/bradfitz/slice"
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
+	smetrics "github.com/xrash/smetrics"
 )
 
 func connectDB() (conn *sql.DB) {
@@ -13,7 +26,7 @@ func connectDB() (conn *sql.DB) {
 	if err != nil {
 		log.WithFields(log.Fields{"error": err.Error}).Error("Failed to open database")
 	} else {
-		log.WithFields(log.Fields{"db": db.Driver}).Info("Opened database")
+		log.WithFields(log.Fields{"DB": db.Driver}).Info("Opened database")
 	}
 
 	return db
@@ -29,9 +42,11 @@ type CFItem struct {
 	ConceptKeywords    sql.NullString `json:"conceptKeywords"`
 	EducationLevel     sql.NullString `json:"educationLevel"`
 	LastChangeDateTime sql.NullString `json:"lastChangeDateTime"`
+	Relevance          int
 }
 
-func getRelatedItems(db *sql.DB, phrase string) {
+func getRelatedItems(db *sql.DB, phrase string) []CFItem {
+
 	// Execute the query
 	sql := "SELECT uri, human_coding_scheme, full_statement, concept_keywords FROM ls_item WHERE full_statement LIKE '%" + phrase + "%';"
 	log.WithFields(log.Fields{"SQL": sql}).Info("Query to execute")
@@ -39,6 +54,8 @@ func getRelatedItems(db *sql.DB, phrase string) {
 	if err != nil {
 		log.WithFields(log.Fields{"err": err.Error}).Fatal("Can't query!") // proper error handling instead of panic in your app
 	}
+
+	var matches []CFItem
 	for results.Next() {
 		var cfItem CFItem
 		// for each row, scan the result into our tag composite object
@@ -49,9 +66,34 @@ func getRelatedItems(db *sql.DB, phrase string) {
 		}
 		// and then print out the tag's Name attribute
 		log.WithFields(log.Fields{"URI": cfItem.URI, "FullStatement": cfItem.FullStatement}).Info("matches phrase")
-
+		matches = append(matches, cfItem)
 	}
+	return matches
+}
 
+func scoreRelevance(cfItem CFItem, phrase string) int {
+	score := smetrics.Ukkonen(cfItem.FullStatement.String, phrase, 1, 1, 2)
+	return score
+}
+
+func itemsByRelevance(matches map[string]CFItem, phrase string) []CFItem {
+	var result []CFItem
+	for _, v := range matches {
+		v.Relevance = scoreRelevance(v, phrase)
+		result = append(result, v)
+	}
+	slice.Sort(result, func(i, j int) bool {
+		return result[i].Relevance > result[j].Relevance
+	})
+	return result
+}
+
+func cfItemSliceToMap(elements []CFItem) map[string]CFItem {
+	elementMap := make(map[string]CFItem)
+	for _, item := range elements {
+		elementMap[item.URI.String] = item
+	}
+	return elementMap
 }
 
 func main() {
@@ -63,6 +105,17 @@ func main() {
 		phrase = os.Args[1]
 	}
 	db := connectDB()
-	getRelatedItems(db, phrase)
+
+	var allMatches []CFItem
+	words := strings.Split(phrase, " ")
+	for _, v := range words {
+		matches := getRelatedItems(db, v)
+		allMatches = append(allMatches, matches...)
+	}
+	itemMap := cfItemSliceToMap(allMatches)
+	allMatches = itemsByRelevance(itemMap, phrase)
 	db.Close()
+	for _, v := range allMatches {
+		log.WithFields(log.Fields{"Relevance": v.Relevance, "URI": v.URI, "FullStatement": v.FullStatement, "Phrase": phrase}).Info("Matches phrase")
+	}
 }
